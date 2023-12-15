05dfb7a173c7b3f6f49f89179502c97a783e4008
EddyBough <boughanmieddy@outlook.fr>
2023-12-15 14:29:28 +0100

    skills component designed and create

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import "./assets/styles/main.scss";
+import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <React.StrictMode>
+  <ThemeProvider>
     <App />
+  </ThemeProvider>
   </React.StrictMode>
   </BrowserRouter>
 )
