3b4f20198055ce95a9e4444f091f8d6849f2a83f
EddyBough <boughanmieddy@outlook.fr>
2024-01-16 15:10:12 +0100

    bg changed and effect on button

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -12,16 +12,20 @@ const Presentation = () => {
           </h3>
           <hr className="custom-hr border-t-2 border-b-2 text-center md:w-1/4 w-1/3 my-4 mx-auto md:mx-0" />
           <p className="mb-4 text-white text-s md:h-auto">
-            Hello everyone, I’m Eddy and I’m a fullstack developer <br /> I have
-            good knowledge and basics in coding <br />I have worked in many
-            amazing project I’m still learning the job. <br /> You can check
-            some of my work lower in the page
+          Hello everyone, I&apos;m Eddy and I&apos;m a full-stack developer. <br />
+  I have a good understanding of programming and have worked on many amazing projects. <br />
+  I&apos;m always learning and growing, which is one of the things I love most about my job. <br /> 
+  You can check out some of my work below.
           </p>
-          <div className="flex space-x-4">
-            <button className="py-2 rounded cursor-pointer">
-              <img src="/public/img/imagebutton.svg" alt="Image" />
-            </button>
-            <p className="text-white mt-10">Click here to see my CV</p>
+          <div className="flex justify-center md:justify-start">
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <div className="w-20 h-20 relative">
+  <div className="custom-button absolute w-full h-full rounded-full micro"></div>
+  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div className="w-0 h-0 border-l-[20px] border-l-transparent border-t-[30px] border-t-white-500 border-r-[20px] border-r-transparent rounded"></div>
+  </div>
+</div></a>
+<p className="text-white m-3 mt-7">Click here to see my CV</p>
           </div>
         </div>
       </div>
@@ -29,7 +33,7 @@ const Presentation = () => {
       {/* Div de droite */}
       <div className="w-full md:w-full">
         <img
-          className="h-60 w-full md:w-full md:h-screen"
+          className="h-60 w-full md:w-full object-cover md:h-screen"
           src="/img/image11.png"
           alt="Image"
         />
