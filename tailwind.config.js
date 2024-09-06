ef0220dc6bd2b808ad00ba92b02b1862ce781802
EddyBough <boughanmieddy@outlook.fr>
2024-09-06 12:32:56 +0200

    component AuroraBackgroundDemo designed and integrated in Presentation component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
-// Utilise la syntaxe ESM avec import et export
-import withMT from "@material-tailwind/react/utils/withMT";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
-export default withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{ts,tsx,jsx,js}"],
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
+        aurora: "aurora 60s linear infinite",
         "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes: {
+        aurora: {
+          from: { backgroundPosition: "50% 50%, 50% 50%" },
+          to: { backgroundPosition: "350% 50%, 350% 50%" },
+        },
         meteor: {
           "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
           "70%": { opacity: "1" },
@@ -20,7 +27,17 @@ export default withMT({
       },
     },
   },
-  plugins: [],
-});
+  plugins: [addVariablesForColors],
+};
 
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
 
+  addBase({
+    ".aurora-effect": newVars,
+    ".meteor-effect": newVars,
+  });
+}
